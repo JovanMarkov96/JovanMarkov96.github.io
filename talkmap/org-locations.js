bfbe8d5d4979c5b0fd45546f2a6de8c73bfa1018
Jovan Markov <13520882+JovanMarkov96@users.noreply.github.com>
2025-02-06 21:04:36 +0200

    Update talk locations to include new venues in Serbia and Spokane, WA

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Petnica, Serbia",
+    44.2599,
+    19.9286
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Belgrade, Serbia",
+    44.8125,
+    20.4612
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Spokane, WA, USA",
+    47.6580,
+    -117.4235
   ]
 ];
